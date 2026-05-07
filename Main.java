a1f7535a8177871020e24c332e46fc44776a865f
Nicolas Lacerda <nicolaslacerdaa@gmail.com>
2026-05-06 23:09:06 -0300

    SOUT
    SOUT dica, comentários e comentário de múltiplas linhas.

--- Main.java
+++ Main.java
@@ -2,5 +2,21 @@ public class Main {
 
     public static void main(String[] args){
 
+
+        //Este é meu primeiro programa em java
+
+        //Comentários são com 2 barras normais
+
+
+        /* Comentário com mais de 1 linha
+         é assim que se faz */
+
+        System.out.println("I Like Pizza\n");
+        System.out.println("It's really good!");
+        System.out.println("Buy me pizza!");
+
+      //apreviação de SYSTEM OUT PRINT É "SOUT"(mágica)
+
+    
     }
 }
