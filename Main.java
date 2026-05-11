71388a02e461f03b52b30b200dd79ce146b9a407
Nicolas Lacerda <nicolaslacerdaa@gmail.com>
2026-05-10 23:33:46 -0300

    Criação do conteúdo variaveis e inclusão de exemplos na main

--- Main.java
+++ Main.java
@@ -11,12 +11,37 @@ public class Main {
         /* Comentário com mais de 1 linha
          é assim que se faz */
 
-        System.out.println("I Like Pizza\n");
-        System.out.println("It's really good!");
-        System.out.println("Buy me pizza!");
+        //System.out.println("I Like Pizza\n");
+        //System.out.println("It's really good!");
+        //System.out.println("Buy me pizza!");
 
       //apreviação de SYSTEM OUT PRINT É "SOUT"(mágica)
 
+
+int age = 21;
+int year = 2024;
+int quantity = 1;
     
+double price = 9.99;
+double temperatura = 9.99;
+double desconto = 9.99;
+
+char grade = 'A';
+char symbol = '!';
+char currency = '$';
+
+
+boolean isStudent = false;
+boolean forSale = false;
+boolean isOnline = false;
+
+
+
+
+    String name = "John";
+    String city = "New York";
+    String food = "Pizza";
+
+    System.out.println("My name is " + name + ", I live in " + city + " and I love " + food + ".");
     }
 }
