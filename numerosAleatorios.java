a5eeeda23c292a06d21b3f50d637dda8b3fb4f0d
Nicolas Lacerda <nicolaslacerdaa@gmail.com>
2026-06-02 23:18:56 -0300

    Criação do campo booleano aleatório
    Criação do campo booleano aleatório
    verdadeiro e falso
    cara ou coroa

--- numerosAleatorios.java
+++ numerosAleatorios.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class numerosAleatorios {
     public static void main(String[] args) {
         // Gerar um número aleatório entre 0 e 1
@@ -11,5 +13,15 @@ public class numerosAleatorios {
         // Gerar um número aleatório entre 50 e 150
         int randomIntRange = (int)(Math.random() * 101) + 50;
         System.out.println("Número aleatório entre 50 e 150: " + randomIntRange);
+
+        Random random = new Random();
+        boolean isTrue = random.nextBoolean();
+
+        if (isTrue == true) {
+            System.out.println("O valor booleano é verdadeiro.");
+        } else {
+            System.out.println("O valor booleano é falso.");
+        }
+
     }
 }
